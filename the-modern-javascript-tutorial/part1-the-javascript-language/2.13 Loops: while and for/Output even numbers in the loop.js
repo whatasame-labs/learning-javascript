822bc22d5541fb995311ae286648dac3b21068fd
Jeonggyu Choi <jeonggyu.choi@navercorp.com>
2025-07-29 14:20:38 +0900

    Add loop examples

---
+++ the-modern-javascript-tutorial/part1-the-javascript-language/2.13 Loops: while and for/Output even numbers in the loop.js
@@ -0,0 +1,5 @@
+for (let number = 1; number <= 10; number++) {
+  if (number % 2 == 0) {
+    alert(number);
+  }
+}
---
+++ the-modern-javascript-tutorial/part1-the-javascript-language/2.13 Loops: while and for/Which values does the while loop show.js
@@ -0,0 +1,5 @@
+let i = 0;
+while (++i < 5) alert(i); // alert 1, 2, 3, 4
+
+let j = 0;
+while (j++ < 5) alert(j); // alert 1, 2, 3, 4, 5
